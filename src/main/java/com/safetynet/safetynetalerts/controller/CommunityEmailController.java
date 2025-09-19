a16878f1feb2905ea411c7702525a1c00da0c97d
NastassjaRosa <nastassja.paulhiac@gmail.com>
2025-09-19 09:45:08 +0200

    GET /communityEmail?city=<city> tests ok postman

--- src/main/java/com/safetynet/safetynetalerts/controller/CommunityEmailController.java
+++ src/main/java/com/safetynet/safetynetalerts/controller/CommunityEmailController.java
@@ -4,8 +4,13 @@ package com.safetynet.safetynetalerts.controller;
 import com.safetynet.safetynetalerts.service.CommunityEmailService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -13,7 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class CommunityEmailController {
 
-private final CommunityEmailService communityEmailService;
+private final CommunityEmailService service;
+
+
+
+    @GetMapping(params = "city")
+    public ResponseEntity<List<String>> getEmails(@RequestParam String city) {
+
+
+        log.debug("GET /communityEmail?city={}", city);
+
+        List<String> emails = service.getEmailsByCity(city);
+        log.info("Emails trouvés!!!!! :{}", emails.size());
+        return ResponseEntity.ok(emails);
+    }
 
 
 }
--- src/main/java/com/safetynet/safetynetalerts/service/CommunityEmailService.java
+++ src/main/java/com/safetynet/safetynetalerts/service/CommunityEmailService.java
@@ -1,14 +1,32 @@
 package com.safetynet.safetynetalerts.service;
 
 
+import com.safetynet.safetynetalerts.model.Person;
+import com.safetynet.safetynetalerts.repository.DataRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class CommunityEmailService {
 
+    private final DataRepository repo;
+
+    public List<String> getEmailsByCity(String city) {
+        log.debug("getEmailsByCity city: {}", city);
+
+        return repo.getDataFile()
+                .getPersons()
+                .stream()
+                .filter(p -> p.getCity().equalsIgnoreCase(city))
+                .map(Person::getEmail)
+                .distinct()
+                .collect(Collectors.toList());
+    }
 
 }
